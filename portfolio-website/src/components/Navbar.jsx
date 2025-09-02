076c030c2174a49bb5c594efc6fa728148aa7598
Lana-Cambeses <lanacambeses@gmail.com>
2025-09-01 19:37:53 -0700

    Tailwind learning video 1

--- portfolio-website/src/components/Navbar.jsx
+++ portfolio-website/src/components/Navbar.jsx
@@ -13,7 +13,6 @@ function Navbar() {
       <ul className="nav-links">
         <li><a href="#">Home</a></li>
         <li><a href="#">Projects</a></li>
-        <li><a href="#">Game Demos</a></li>
         <li><a href="#">About</a></li>
         <li><a href="#">Contact</a></li>
       </ul>
